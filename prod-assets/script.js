3cc44cf5a95ba23cbb4a20ce2e045187e5442fb2
Maciej Preus <mpreus@onet.eu>
2019-08-28 22:04:38 +0200

    Add star-rating element to the product1.html site

--- prod-assets/script.js
+++ prod-assets/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-	console.log("It works!");
+	let stars = $("#rating .product1-star");
+	let radios = $(":radio[name='rating']");
+
+	stars.on("mouseenter", function() {
+		$(this).prevAll().addBack().addClass("hovered");
+	});
+
+	stars.on("mouseleave", function() {
+		$(this).prevAll().addBack().removeClass("hovered");
+		}
+	);
+
+	stars.on("click", function() {
+		radios.eq($(this).index()).prop("checked", true);
+
+		$(this).siblings().removeClass("selected");
+		$(this).prevAll().addBack().addClass("selected");
+
+	});
+
+
+
+
 })
