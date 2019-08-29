40346962231855a2d894cfb9481415e8d5fa2483
Maciej Preus <mpreus@onet.eu>
2019-08-29 20:52:26 +0200

    Add jQuery functionalities to about.html site

--- prod-assets/script.js
+++ prod-assets/script.js
@@ -20,6 +20,29 @@ $(document).ready(function() {
 		$(this).prevAll().addBack().addClass("selected");
 	});
 
+
+/* about the company - history 'read more / read less' */
+	let historyTrigger = $(".read-more-span"),
+		historyContent = $(".read-more-para");
+
+	historyTrigger.on("click", function() {
+		historyContent.fadeToggle(1200);
+		if ( historyTrigger.text() === "read more..." ) {
+			historyTrigger.text("read less...");
+		} else {
+			historyTrigger.text("read more...");
+		}
+	});
+
+/* our values - show & hide */
+	let valueTitle = $("h4.values-title");
+
+	valueTitle.on("click", function() {
+		$(this).next().toggle(700);
+	})
+	
+
+
 /* another elements below */
 
 
